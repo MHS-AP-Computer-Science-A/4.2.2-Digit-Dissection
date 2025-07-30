b0621d76431b228b7a2eb8abae3a06ff43e8bccb
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2025-07-30 08:43:56 -0400

    Update DigitDissection.java

--- src/DigitDissection.java
+++ src/DigitDissection.java
@@ -1,37 +1,29 @@
-
-import java.util.Random;
-
 public class DigitDissection {
 
-    /**
-     * Returns the number of digits in the given number.
-     * Postcondition: result >= 1 (since number has at least one digit)
-     */
+    // Postcondition: Returns the number of digits in the given number.
     public static int countDigits(int number) {
         // TODO: Implement this method
         return 0;
     }
 
-    /**
-     * Returns the sum of the even digits in the given number.
-     * Postcondition: result >= 0
-     */
+    // Postcondition: Returns the sum of the even digits in the given number.
     public static int sumEvenDigits(int number) {
         // TODO: Implement this method
         return 0;
     }
 
-    /**
-     * Returns the average of the digits in the given number.
-     * Postcondition: result is >= 0.0 and <= 9.0
-     */
+    // Postcondition: Returns the average of the digits in the given number.
     public static double averageOfDigits(int number) {
         // TODO: Implement this method
         return 0.0;
     }
 
+
+
     public static void main(String[] args) {
-        int testNum = (int)(Math.random() * 999991) + 10; // Random number between 10 and 1000000
+        Random rand = new Random();
+
+        int testNum = rand.nextInt(99991) + 10; // Random number between 10 and 100000
         System.out.println("Random number: " + testNum);
 
         System.out.println("Number of digits: " + countDigits(testNum));
